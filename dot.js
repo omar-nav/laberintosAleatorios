819cac5f18dee72b4d871ec4d7b1594215111bc3
omar-nav <omar.navarro@g.austincc.edu>
2018-09-06 17:17:05 -0500

    add final game with sounds

---
+++ dot.js
@@ -0,0 +1,351 @@
+var canvas = document.getElementById("someCanvas");
+var context = canvas.getContext("2d");
+
+var finalMessage = document.getElementById("container");
+var frames = 0;
+// global variables
+var interval;
+var images = {
+  pinkDot: "./images/pinkDot.png",
+  blueDot: "./images/blueDot.svg",
+  ironHack: "./images/IronHackLogo.png"
+};
+
+class Maze {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.onload = () => {
+      this.draw();
+    };
+
+    this.music = new Audio();
+    this.music.src = "./sounds/Jackson5.mp3";
+  }
+  draw() {
+    mazeReader();
+  }
+}
+class Dot {
+  constructor(x, y, image) {
+    this.x = x; // 209
+    this.y = y; // 129
+    this.width = 7; // 9
+    this.height = 7; // 9
+    this.touchingWall = false;
+    this.culpritKey = 0;
+    this.lastKey = 0;
+    this.image = new Image();
+    this.image.src = image;
+    this.image.onload = () => {
+      this.draw();
+    };
+  }
+  draw() {
+    context.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+
+  touchWith(item) {
+    var touch =
+      this.x < item[0] + item[2] &&
+      this.x + this.width > item[0] &&
+      this.y < item[1] + item[3] &&
+      this.y + this.height > item[1];
+
+    if (touch) {
+      //pink keys are 68, 65, 87, 83
+      if (!this.culpritKey == this.lastKey) {
+        this.culpritKey = 0;
+        this.touchingWall = false;
+      } else {
+        this.culpritKey = this.lastKey;
+        this.touchingWall = true;
+        if (this.culpritKey === 68) {
+          this.x -= 2;
+        }
+        if (this.culpritKey === 65) {
+          this.x += 2;
+        }
+        if (this.culpritKey === 87) {
+          this.y += 2;
+        }
+        if (this.culpritKey === 83) {
+          this.y -= 2;
+        }
+        // blue starts
+        if (this.culpritKey === 39) {
+          this.x -= 2;
+        }
+        if (this.culpritKey === 37) {
+          this.x += 2;
+        }
+        if (this.culpritKey === 40) {
+          this.y -= 2;
+        }
+        if (this.culpritKey === 38) {
+          this.y += 2;
+        }
+        this.culpritKey = 0;
+        this.touchingWall = false;
+      }
+    }
+  }
+}
+
+class Prize {
+  constructor(x, y, image) {
+    this.x = x;
+    this.y = y;
+    this.width = 23;
+    this.height = 10;
+    this.touchingWall = false;
+    this.image = new Image();
+    this.image.src = image;
+    this.image.onload = () => {
+      this.draw();
+    };
+    this.touch = new Audio();
+    this.touch.src = "./sounds/JSnoTeEspera.m4a";
+  }
+  draw() {
+    context.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+  touchWith(item) {
+    var touch =
+      this.x < item.x + item.width &&
+      this.x + this.width > item.x &&
+      this.y < item.y + item.height &&
+      this.y + this.height > item.y;
+    if (touch) {
+      this.touch.play();
+      return true;
+    }
+    return false;
+  }
+}
+// instancias
+let backgroundMaze = new Maze();
+let pink = new Dot(9, 4, images.pinkDot);
+let blue = new Dot(209, 129, images.blueDot);
+let logo = new Prize(102, 59, images.ironHack);
+
+function update() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  backgroundMaze.draw();
+  frames++;
+  pink.draw();
+  blue.draw();
+  logo.draw();
+  checkTouch();
+  checkvals();
+  //pink keys are 68, 65, 87, 83
+  if (
+    backgroundMaze.keys &&
+    backgroundMaze.keys[68] &&
+    pink.touchingWall === false
+  ) {
+    pink.x += 0.75;
+    pink.lastKey = 68;
+  }
+  if (
+    backgroundMaze.keys &&
+    backgroundMaze.keys[65] &&
+    pink.touchingWall === false
+  ) {
+    pink.x -= 0.75;
+    pink.lastKey = 65;
+  }
+  if (
+    backgroundMaze.keys &&
+    backgroundMaze.keys[87] &&
+    pink.touchingWall === false
+  ) {
+    pink.y -= 0.75;
+    pink.lastKey = 87;
+  }
+  if (
+    backgroundMaze.keys &&
+    backgroundMaze.keys[83] &&
+    pink.touchingWall === false
+  ) {
+    pink.y += 0.75;
+    pink.lastKey = 83;
+  }
+  // pink hits wall
+  if (
+    backgroundMaze.keys &&
+    pink.culpritKey === 68 &&
+    pink.touchingWall === true
+  ) {
+    pink.x += 0;
+    pink.lastKey = 68;
+  }
+  if (
+    backgroundMaze.keys &&
+    pink.culpritKey === 65 &&
+    pink.touchingWall === true
+  ) {
+    pink.x -= 0;
+    pink.lastKey = 65;
+  }
+  if (
+    backgroundMaze.keys &&
+    pink.culpritKey === 87 &&
+    pink.touchingWall === true
+  ) {
+    pink.y += -0;
+    pink.lastKey = 87;
+  }
+  if (
+    backgroundMaze.keys &&
+    pink.culpritKey === 83 &&
+    pink.touchingWall === true
+  ) {
+    pink.y += 0;
+    pink.lastKey = 83;
+  }
+
+  if (
+    backgroundMaze.keys &&
+    backgroundMaze.keys[39] &&
+    blue.touchingWall === false
+  ) {
+    blue.x += 0.75;
+    blue.lastKey = 39;
+  }
+  if (
+    backgroundMaze.keys &&
+    backgroundMaze.keys[37] &&
+    blue.touchingWall === false
+  ) {
+    blue.x -= 0.75;
+    blue.lastKey = 37;
+  }
+  if (
+    backgroundMaze.keys &&
+    backgroundMaze.keys[40] &&
+    blue.touchingWall === false
+  ) {
+    blue.y += 0.75;
+    blue.lastKey = 40;
+  }
+  if (
+    backgroundMaze.keys &&
+    backgroundMaze.keys[38] &&
+    blue.touchingWall === false
+  ) {
+    blue.y -= 0.75;
+    blue.lastKey = 38;
+  }
+  // pink hits wall
+  if (
+    backgroundMaze.keys &&
+    blue.culpritKey === 39 &&
+    blue.touchingWall === true
+  ) {
+    blue.x += 0;
+    blue.lastKey = 39;
+  }
+  if (
+    backgroundMaze.keys &&
+    blue.culpritKey === 37 &&
+    blue.touchingWall === true
+  ) {
+    blue.x -= 0;
+    blue.lastKey = 37;
+  }
+  if (
+    backgroundMaze.keys &&
+    blue.culpritKey === 87 &&
+    blue.touchingWall === true
+  ) {
+    blue.y += -0;
+    blue.lastKey = 87;
+  }
+  if (
+    backgroundMaze.keys &&
+    blue.culpritKey === 83 &&
+    blue.touchingWall === true
+  ) {
+    blue.y += 0;
+    blue.lastKey = 83;
+  }
+}
+
+function start() {
+  if (interval) return;
+  frames = 0;
+  interval = setInterval(update, 60 / 60);
+  window.addEventListener("keydown", function(e) {
+    backgroundMaze.keys = backgroundMaze.keys || [];
+    backgroundMaze.keys[e.keyCode] = true;
+  });
+  window.addEventListener("keyup", function(e) {
+    backgroundMaze.keys[e.keyCode] = false;
+  });
+}
+
+function checkvals() {
+  if (frames % 100 === 0) {
+    console.log(pink.touchingWall);
+  }
+}
+
+function checkTouch() {
+  oddRowCtx.forEach(function(lineOrSquare) {
+    pink.touchWith(lineOrSquare);
+  });
+  oddRowCtx.forEach(function(lineOrSquare) {
+    blue.touchWith(lineOrSquare);
+  });
+  evenRowCtx.forEach(function(lineOrSquare) {
+    pink.touchWith(lineOrSquare);
+  });
+  evenRowCtx.forEach(function(lineOrSquare) {
+    blue.touchWith(lineOrSquare);
+  });
+  if (logo.touchWith(pink)) {
+    gameOver("ROSA");
+    // temporary fix for styling bug
+  }
+  if (logo.touchWith(blue)) {
+    gameOver("AZUL");
+  }
+}
+
+function gameOver(winnerNameString) {
+  clearInterval(interval);
+  let winner = winnerNameString;
+  if (winner === "ROSA") {
+    context.fillStyle = "pink";
+  } else {
+    context.fillStyle = "LightBlue";
+  }
+
+  var message = `¡HA GANADO ${winner} !`;
+  var message2 = "Retrocede la página para crear otro laberinto";
+  var message3 = "Refresca la página para jugar con el mismo laberinto";
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.font = "bold 25px Indie Flower";
+
+  context.fillText(message, 0, 30);
+  context.font = "bold 12px Indie Flower";
+  context.fillText(message2, 0, 60);
+  context.fillText(message3, 0, 90);
+
+  interval = null;
+  backgroundMaze.music.pause();
+}
+
+addEventListener("keydown", function(e) {
+  if (e.keyCode === 27) {
+    start();
+  }
+
+  if ((e.keyCode = "Enter")) {
+    start();
+    backgroundMaze.music.play();
+  }
+});
---
+++ mazeReader.js
@@ -0,0 +1,293 @@
+var canvas = document.getElementById("someCanvas");
+var ctx = canvas.getContext("2d");
+
+// an array of objects
+let oddRowCtx = [];
+let evenRowCtx = [];
+
+function storeMaze() {
+  let mazeText = [];
+  mazeText = document.getElementById("out").innerHTML;
+  localStorage.setItem("mazeTextCookie", mazeText);
+}
+
+function mazeReader() {
+  // organize giant array into rows of arrays
+  let mazeText = localStorage.getItem("mazeTextCookie");
+  let mazeArray = mazeText.split("");
+  let x = 0;
+  let y = 0;
+
+  let organizedMazeArray = [];
+
+  let row0 = [];
+  let row1 = [];
+  let row2 = [];
+  let row3 = [];
+  let row4 = [];
+  let row5 = [];
+  let row6 = [];
+  let row7 = [];
+  let row8 = [];
+  let row9 = [];
+  let row10 = [];
+  let row11 = [];
+  let row12 = [];
+  let row13 = [];
+  let row14 = [];
+  let row15 = [];
+  let row16 = [];
+  let row17 = [];
+  let row18 = [];
+  let row19 = [];
+  let row20 = [];
+
+  // let rowTwo = [];
+  // for each element partition rows into objects
+  mazeArray.forEach(function(element, index) {
+    // each row is 45 characters in length
+    //get row 0
+    if (index < 45) {
+      row0.push(element);
+    }
+    if (index === 45) {
+      organizedMazeArray.push(row0);
+    }
+    // get row 1 and skip the enter
+    if (index > 45 && index < 91) {
+      row1.push(element);
+    }
+    if (index === 91) {
+      organizedMazeArray.push(row1);
+    }
+    if (index > 91 && index < 137) {
+      row2.push(element);
+    }
+    if (index === 137) {
+      organizedMazeArray.push(row2);
+    }
+    if (index > 137 && index < 183) {
+      row3.push(element);
+    }
+    if (index === 183) {
+      organizedMazeArray.push(row3);
+    }
+    if (index > 183 && index < 229) {
+      row4.push(element);
+    }
+    if (index === 229) {
+      organizedMazeArray.push(row4);
+    }
+    if (index > 229 && index < 275) {
+      row5.push(element);
+    }
+    if (index === 275) {
+      organizedMazeArray.push(row5);
+    }
+    if (index > 275 && index < 321) {
+      row6.push(element);
+    }
+    if (index === 321) {
+      organizedMazeArray.push(row6);
+    }
+    if (index > 321 && index < 367) {
+      row7.push(element);
+    }
+    if (index === 367) {
+      organizedMazeArray.push(row7);
+    }
+    if (index > 367 && index < 413) {
+      row8.push(element);
+    }
+    if (index === 413) {
+      organizedMazeArray.push(row8);
+    }
+    if (index > 413 && index < 459) {
+      row9.push(element);
+    }
+    if (index === 459) {
+      organizedMazeArray.push(row9);
+    }
+    if (index > 459 && index < 505) {
+      row10.push(element);
+    }
+    if (index === 505) {
+      organizedMazeArray.push(row10);
+    }
+    if (index > 505 && index < 551) {
+      row11.push(element);
+    }
+    if (index === 551) {
+      organizedMazeArray.push(row11);
+    }
+    if (index > 551 && index < 597) {
+      row12.push(element);
+    }
+    if (index === 597) {
+      organizedMazeArray.push(row12);
+    }
+    if (index > 597 && index < 643) {
+      row13.push(element);
+    }
+    if (index === 643) {
+      organizedMazeArray.push(row13);
+    }
+    if (index > 643 && index < 689) {
+      row14.push(element);
+    }
+    if (index === 689) {
+      organizedMazeArray.push(row14);
+    }
+    if (index > 689 && index < 735) {
+      row15.push(element);
+    }
+    if (index === 735) {
+      organizedMazeArray.push(row15);
+    }
+    if (index > 735 && index < 781) {
+      row16.push(element);
+    }
+    if (index === 781) {
+      organizedMazeArray.push(row16);
+    }
+    if (index > 781 && index < 827) {
+      row17.push(element);
+    }
+    if (index === 827) {
+      organizedMazeArray.push(row17);
+    }
+    if (index > 827 && index < 873) {
+      row18.push(element);
+    }
+    if (index === 873) {
+      organizedMazeArray.push(row18);
+    }
+    if (index > 873 && index < 919) {
+      row19.push(element);
+    }
+    if (index === 919) {
+      organizedMazeArray.push(row19);
+    }
+    if (index > 919 && index < 965) {
+      row20.push(element);
+    }
+    if (index === 965) {
+      organizedMazeArray.push(row20);
+    }
+  });
+  // console.log(organizedMazeArray);
+
+  // redraw rows with ctx
+  function drawOddRow(rowArray) {
+    ctx.beginPath();
+    // ctx.lineWidth = "3";
+    // ctx.strokeStyle = "black";
+    rowArray.forEach(function(element, index) {
+      // draw square
+      // ctx.fillRect(x,y,color,width, height)
+      if (element === "◾") {
+        // oddRowCtx.push([x, y, 5, 3.478]);
+        oddRowCtx.push([x, y, 5, 3.478]);
+        ctx.fillStyle = "#A66D3F";
+        ctx.fillRect(x, y, 5, 3.478);
+        x += 5;
+      }
+      if (element === " ") {
+        //oddRowCtx.push(["air"]);
+        x += 5;
+      }
+      if (element === "—") {
+        oddRowCtx.push([x, y, 5, 3.478]);
+        ctx.fillStyle = "#ffccff";
+        ctx.fillRect(x, y, 5, 3.478);
+        x += 5;
+      }
+    });
+  }
+  function drawEvenRow(rowArray) {
+    ctx.beginPath();
+    ctx.lineWidth = "3";
+    ctx.strokeStyle = "black";
+    rowArray.forEach(function(element, index) {
+      if (element === " ") {
+        //evenRowCtx.push(["air"]);
+        x += 5;
+      }
+      if (element === "|") {
+        evenRowCtx.push([x, y, 5, 10.434]);
+        ctx.fillStyle = "#ffccff";
+        // ctx.fillRect(x,y,width, height)
+        ctx.fillRect(x, y, 5, 10.434);
+        x += 5;
+      }
+    });
+  }
+
+  // store arrays without ↵  character
+  // draw first row
+  drawOddRow(organizedMazeArray[0]);
+  x = 0;
+  y += 3.478;
+  drawEvenRow(organizedMazeArray[1]);
+  x = 0;
+  y += 10.434;
+  drawOddRow(organizedMazeArray[2]);
+  x = 0;
+  y += 3.478;
+  drawEvenRow(organizedMazeArray[3]);
+  x = 0;
+  y += 10.434;
+  drawOddRow(organizedMazeArray[4]);
+  x = 0;
+  y += 3.478;
+  drawEvenRow(organizedMazeArray[5]);
+  x = 0;
+  y += 10.434;
+  drawOddRow(organizedMazeArray[6]);
+  x = 0;
+  y += 3.478;
+  drawEvenRow(organizedMazeArray[7]);
+  x = 0;
+  y += 10.434;
+  drawOddRow(organizedMazeArray[8]);
+  x = 0;
+  y += 3.478;
+  drawEvenRow(organizedMazeArray[9]);
+  x = 0;
+  y += 10.434;
+  drawOddRow(organizedMazeArray[10]);
+  x = 0;
+  y += 3.478;
+  drawEvenRow(organizedMazeArray[11]);
+  x = 0;
+  y += 10.434;
+  drawOddRow(organizedMazeArray[12]);
+  x = 0;
+  y += 3.478;
+  drawEvenRow(organizedMazeArray[13]);
+  x = 0;
+  y += 10.434;
+  drawOddRow(organizedMazeArray[14]);
+  x = 0;
+  y += 3.478;
+  drawEvenRow(organizedMazeArray[15]);
+  x = 0;
+  y += 10.434;
+  drawOddRow(organizedMazeArray[16]);
+  x = 0;
+  y += 3.478;
+  drawEvenRow(organizedMazeArray[17]);
+  x = 0;
+  y += 10.434;
+  drawOddRow(organizedMazeArray[18]);
+  x = 0;
+  y += 3.478;
+  drawEvenRow(organizedMazeArray[19]);
+  x = 0;
+  y += 10.434;
+  drawOddRow(organizedMazeArray[20]);
+}
+
+// setTimeout(function() {
+//   storeMaze();
+// }, 2000);
